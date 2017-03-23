bd66dba799bdfe9c6de7f2720ae284e619cfb4d1
GimoMeng <375858470@qq.com>
2017-03-23 11:13:30 +0800

    wrote a file

---
+++ PythonDay03/xiti1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print("n = 123" );
+print("f = 456.789");
+print("s1 = 'Hello,world'");
+print("s2 = 'Hello,\\'Adam\\''");
+print("s3 = r'Hello, \"Bart\"'");
+print("s4 = r'''Hello,");
+print("Lisa!'''");
---
+++ PythonDay03/xiti2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+s1 = 72
+s2 = 85
+
+r = 100*(s2-s1)/s1
+print('提高了%.1f%%' % r);
