0f5ecc57c67cbdfe570cb01117856c876887a454
GimoMeng <375858470@qq.com>
2017-04-03 22:55:17 +0800

    do an exercise

--- PythonDay10/do_generator_xiti.py
+++ PythonDay10/do_generator_xiti.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 #把杨辉三角每一行看做一个list，试写一个generator，不断输出下一行的list：
+#(1)
 def triangles():
  L = [1]
  while True:
-     yield L
-     L = [1] + [L[i-1]+L[i] for i in range(len(L)) if i > 0] + [1]
+    yield L
+    L = [1] + [L[i-1]+L[i] for i in range(len(L)) if i > 0] + [1]
+
