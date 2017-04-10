ae369a40578fce0a0f9d0d94e73a7042ee6b5c1e
GimoMeng <375858470@qq.com>
2017-04-10 21:16:18 +0800

    do an exercise

--- PythonDay10/do_sorted_xiti.py
+++ PythonDay10/do_sorted_xiti.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#假设我们用一组tuple表示学生名字和成绩：
+#L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
 def by_name(t):
