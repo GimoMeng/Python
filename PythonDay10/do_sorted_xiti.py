e0d582c540bdedb2093c82693764757ddae8c98c
GimoMeng <375858470@qq.com>
2017-04-10 21:18:06 +0800

    do an exercise

--- PythonDay10/do_sorted_xiti.py
+++ PythonDay10/do_sorted_xiti.py
@@ -10,3 +10,12 @@ def by_name(t):
 
 L2 = sorted(L, key = by_name)
 print(L2)
+
+#再按成绩从高到低排序：
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+def by_score(t):
+    return t[1]
+
+L2 = sorted(L, key = by_score, reverse = True)
+print(L2)
