4ea331a4ee62850754051ddb97ab649dc49dc3cd
GimoMeng <375858470@qq.com>
2017-04-14 18:49:43 +0800

    wrote a file

---
+++ PythonDay01/favorites.py
@@ -0,0 +1,11 @@
+#把你的爱好列出来，并把这个列表起个变量名games.现在把你喜欢的食物列出来，起个变量名为foods.把这两个列表连在一起并把结果命名为favorites.最后，把变量favorites打印出来
+games = ['computer','phone','ball']
+foods = ['apple','banana','pear']
+favorites = games + foods
+print(favorites)
+
+
+#创建两个变量：一个指向你的姓一个指向你的名.创建一个字符串，用占位符使用这两个变量来打印带有你名字的信息，比如“你好,某某某！”
+name1 = 'song'
+name2 = 'meng'
+print('Hello,%s' % name1+name2,'!')
