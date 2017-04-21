93da3122ca4119a87d5a8bbaa52b9b452db50e1f
GimoMeng <375858470@qq.com>
2017-04-21 21:58:30 +0800

    do an exercise

--- PythonDay01/practice.py
+++ PythonDay01/practice.py
@@ -306,3 +306,44 @@ class Giraffes():
 
 reginald = Giraffes()
 reginald.funcDance()
+
+
+
+
+#使用4只Pen对象的turtle画出一个叉子
+import time
+import turtle
+
+#线1
+t1 = turtle.Pen()
+t1.forward(100)
+t1.left(90)
+t1.forward(50)
+t1.right(90)
+t1.forward(50)
+
+#线2
+t2 = turtle.Pen()
+t2.forward(100)
+t2.right(90)
+t2.forward(50)
+t2.left(90)
+t2.forward(50)
+
+#线3
+t3 = turtle.Pen()
+t3.forward(120)
+t3.left(90)
+t3.forward(25)
+t3.right(90)
+t3.forward(20)
+
+#线4
+t4 = turtle.Pen()
+t4.forward(120)
+t4.right(90)
+t4.forward(25)
+t4.left(90)
+t4.forward(20)
+
+time.sleep(5)
