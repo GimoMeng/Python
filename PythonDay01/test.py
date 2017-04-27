bd5ec5f7d40d88b86ef79006efb659f0348bd114
GimoMeng <375858470@qq.com>
2017-04-27 20:53:20 +0800

    do an exercise

--- PythonDay01/test.py
+++ PythonDay01/test.py
@@ -1,3 +1,4 @@
+#猜数游戏
 import random
 num = random.randint(1,100)
 while True:
@@ -11,3 +12,25 @@ while True:
             print('Try higher')
     elif i > num:
             print('Try lower')
+
+
+
+
+#画星星
+import turtle
+t = turtle.Pen()
+t.reset
+for x in range(1,9):
+    t.forward(100)
+    t.left(225)
+t.reset
+for x in range(1,38):
+    t.forward(100)
+    t.left(175)
+t.reset
+for x in range(1,19):   #创建一个运行18次的循环
+    t.forward(100)      #向前移动100个像素
+    if x % 2 == 0:      #检查变量x是否包含一个偶数
+        t.left(175)     #左转175度
+    else:               #否则
+        t.left(225)     #左转225度
