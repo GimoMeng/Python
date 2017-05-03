508d7ebffab781e373deef644e92429f1b99333d
GimoMeng <375858470@qq.com>
2017-05-03 22:24:06 +0800

    do an exercise

--- PythonDay01/test.py
+++ PythonDay01/test.py
@@ -39,8 +39,10 @@ for x in range(1,19):   #创建一个运行18次的循环
 
 
 #画盒子
-import tkinter as tk
-base = tk.TK()
+import time
+from tkinter import *
+
+tk = Tk()
 canvas = Canvas(tk,width=400,height=400)
 canvas.pack()
 canvas.create_rectangle(10,10,50,50)
