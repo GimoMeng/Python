1f61912e763a4051a6c546220d1770c3185b0089
GimoMeng <375858470@qq.com>
2017-05-05 21:39:27 +0800

    do an exercise

--- PythonDay01/test.py
+++ PythonDay01/test.py
@@ -57,9 +57,9 @@ import random
 tk = Tk()
 canvas = Canvas(tk,width=400,height=400)
 canvas.pack()
-def random_rectangle(width,height):
+def random_rectangle(width,height,fill_color):
     x1 = random.randrange(width)
     y1 = random.randrange(height)
-    x2 = x1 + random.randrange(width)
-    y2 = y1 + random.randrange(height)
-    canvas.create_rectangle(x1,y1,x2,y2)
+    x2 = random.randrange(x1 + random.randrange(width))
+    y2 = random.randrange(y1 + random.randrange(height))
+    canvas.create_rectangle(x1,y1,x2,y2,fill=fill_color)
