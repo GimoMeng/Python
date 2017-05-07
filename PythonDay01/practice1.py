956aba853cd60b356c70a546d3fb5ec903459e0a
GimoMeng <375858470@qq.com>
2017-05-07 20:44:12 +0800

    do an exercise

--- PythonDay01/practice1.py
+++ PythonDay01/practice1.py
@@ -7,3 +7,11 @@ canvas.create_arc(10,80,200,160,extent=90,style=ARC)
 canvas.create_arc(10,160,200,240,extent=135,style=ARC)
 canvas.create_arc(10,240,200,320,extent=180,style=ARC)
 canvas.create_arc(10,320,200,400,extent=359,style=ARC)
+
+
+from tkinter import *
+tk = Tk()
+canvas = Canvas(tk,width=400,height=400)
+canvas.pack()
+canvas.create_polygon(10,10,100,10,100,110,fill="",outline="black")
+canvas.create_polygon(200,10,240,30,120,100,140,120,fill="",outline="black")
