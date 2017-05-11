f895ac5bbaccb34a8ea5787e1aa69318fec28780
GimoMeng <375858470@qq.com>
2017-05-11 20:29:35 +0800

    do an exercise

--- PythonDay02/Pinball_game.py
+++ PythonDay02/Pinball_game.py
@@ -7,9 +7,34 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10,10,25,25,fill=color)
         self.canvas.move(self.id,245,100)
-    def draw(self):
-        self.canvas.move(self.id,0,-1)
+        starts = [-3,-2,-1,1,2,3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
         
+    def draw(self):
+        self.canvas.move(self.id,self.x,self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <=0:
+            self.y = 3
+        if pos[3] >=self.canvas_height:
+            self.y = -3
+        if pos[0] <=0:
+            self.x = 3
+        if pos[2] >=self.canvas_width:
+            self.x = -3
+
+class Paddle:
+    def __init__(elf,canvas,color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0,0,100,10,fill=color)
+        self.canvas.move(self.id,200,300)
+
+    def draw(self):
+        pass
+
 tk = Tk()
 tk.title("Game")
 tk.resizable(0,0)
