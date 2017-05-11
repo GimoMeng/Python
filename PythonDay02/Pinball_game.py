8e302aaa3ea0efbad751db04062f9952d36a6722
GimoMeng <375858470@qq.com>
2017-05-11 21:40:13 +0800

    do an exercise

--- PythonDay02/Pinball_game.py
+++ PythonDay02/Pinball_game.py
@@ -37,7 +37,7 @@ class Ball:
             self.x = -3
 
 class Paddle:
-    def __init__(elf,canvas,color):
+    def __init__(self,canvas,color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0,0,100,10,fill=color)
         self.canvas.move(self.id,200,300)
