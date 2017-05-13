b2139be1927fd1ffa3c243e5b37600c575ae5811
GimoMeng <375858470@qq.com>
2017-05-13 22:19:27 +0800

    do an exercise

---
+++ PythonDay03/Stickman_games.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import random
+import time
+
+class Game:
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.title("Mr.Stick Man Races for the Exit")
+        self.tk.resizable(0,0)
+        self.tk.wm_attributes("-topmost",1)
+        self.canvas = Canvas(self.tk,width=500,height=500,\
+                highlightthickness=0)
+        self.canvas.pack()
+        self.tk.update()
+        self.canvas_height = 500
+        self.canvas_width = 500
