39693a910072c779d38418cb664c03357f617657
GimoMeng <375858470@qq.com>
2017-05-14 21:52:32 +0800

    do an exercise

--- PythonDay03/Stickman_games.py
+++ PythonDay03/Stickman_games.py
@@ -14,3 +14,11 @@ class Game:
         self.tk.update()
         self.canvas_height = 500
         self.canvas_width = 500
+        self.bg = PhotoImage(file="background.gif")
+        w = self.bg.width()
+        h = self.bg.height()
+        for x in range(0,5):
+            for y in range(0,5):
+                self.canvas.create_image(x * w,y * h,image=self.bg,anchor='nw')
+        self.sprites = []
+        self.running = True
