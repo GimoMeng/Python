3a984e57b0b49ccd2d01c33954be6f344ab9426c
GimoMeng <375858470@qq.com>
2017-05-22 21:46:49 +0800

    do an exercise

--- PythonDay03/Stickman_games.py
+++ PythonDay03/Stickman_games.py
@@ -41,12 +41,21 @@ class Coords:
         self.y2 = y2
 
 def within_x(co1,co2):
-        if co1.x1 > co2.x1 and co1.x1 < co2.x2 \
-               or (co1.x2 > co2.x1 and co1.x2 < co2.x2) \
-               or (co2.x1 > co1.x1 and co2.x1 < co1.x2) \
-               or (co2.x2 > co1.x1 and co2.x2 < co1.x1):
-           return True
-        else:
-            return False
+    if co1.x1 > co2.x1 and co1.x1 < co2.x2 \
+           or (co1.x2 > co2.x1 and co1.x2 < co2.x2) \
+           or (co2.x1 > co1.x1 and co2.x1 < co1.x2) \
+           or (co2.x2 > co1.x1 and co2.x2 < co1.x1):
+        return True
+    else:
+        return False
+
+def within_y(co1,co2):
+    if co1.y1 > co2.y1 and co1.y1 < co2.y2 \
+           or (co1.y2 > co2.y1 and co1.y2 < co2.y2) \
+           or (co2.y1 > co1.y1 and co2.y1 < co1.y2) \
+           or (co2.y2 > co1.y1 and co2.y2 < co1.y1):
+        return True
+    else:
+        return False
 g = Game()
 g.mainloop()
