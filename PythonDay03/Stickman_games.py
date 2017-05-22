57024139b8b957f55a3899655cfac13c9fc44bb5
GimoMeng <375858470@qq.com>
2017-05-22 21:55:02 +0800

    do an exercise

--- PythonDay03/Stickman_games.py
+++ PythonDay03/Stickman_games.py
@@ -63,5 +63,24 @@ def collided_left(co1,co2):
         if co1.x1 <= co2.x2 and co1.x1 >= co2.x1:
             return True
     return False
+
+def collided_right(co1,co2):
+    if within_y(co1,co2):
+        if co1.x2 >= co2.x1 and co1.x2 <= co2.x2:
+            return True
+    return False
+
+def collided_top(co1,co2):
+    if within_x(co1,co2):
+        if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
+            return True
+    return False
+
+def collided_bottom(y,co1,co2):
+    if within_x(co1,co2):
+        y_calc = co1.y2 + y
+        if y_calc >= co2.y1 and y_calc <= co2.y2:
+            return True
+    return False
 g = Game()
 g.mainloop()
